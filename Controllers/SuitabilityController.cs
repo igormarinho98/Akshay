eec98d4a5b738283fef9ff2408cac473816a71e7
Igor Marinho <igormarinholeo2014@gmail.com>
2023-07-30 10:33:57 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/SuitabilityController.cs
@@ -0,0 +1,30 @@
+﻿namespace Akshay.Controllers;
+
+using Akshay.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+
+        [Route("[controller]")]
+        [ApiController]
+        public class SuitabilityController : ControllerBase
+        {
+            private static List<Suitability> suitabilityList = new List<Suitability>();
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(suitabilityList);
+            
+        }
+
+        [HttpPost]
+        public IActionResult AddSuitability(Suitability suitability)
+        {
+            suitabilityList.Add(suitability);
+            return CreatedAtAction(nameof(GetAll), null);
+        }
+
+
+    }
---
+++ Models/Suitability.cs
@@ -0,0 +1,14 @@
+﻿namespace Akshay.Models
+{
+    public class Suitability
+    {
+        int Id { get; set; }
+        public string Perfil { get; set; }
+
+        public string Descricao { get; set; }
+
+        public DateTime DataRegistro { get; set; }
+
+
+     }
+}
