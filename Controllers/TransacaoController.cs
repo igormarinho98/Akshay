036995690ab97a43f5925096952aea2654519969
Igor Marinho <igormarinholeo2014@gmail.com>
2023-08-07 20:07:28 -0300

    Implementado Controller que relaciona info de investimento e investidor.

--- Controllers/TransacaoController.cs
+++ Controllers/TransacaoController.cs
@@ -38,18 +38,22 @@ namespace Akshay.Controllers
             }
 
         }
-        /*
-        [HttpGet]
+        
+        [HttpGet("TransacaoInvest")]
         public async Task<IActionResult> GetTransactionWithInvestor()
         {
             using (var sqlConnection = new SqlConnection(_connectionString))
             {
-                const string sql = "SELECT Transactions.id, Transactions.investment_id, Transactions.transaction_date, Transactions.transaction_amount, " +
-                    "Transactions.transaction_type, Transactions.trader Products.name, Products.description, Products.rentability, " +
-                    "Products.category_id FROM Transactions INNER JOIN Products ON Products.ID = Transactions.product_id";
+                const string sql = "SELECT Transactions.id, Transactions.investment_id, Transactions.transaction_date, Transactions.transaction_amount, Transactions.transaction_type, Transactions.trader, Products.name, Products.description, Products.rentability, Products.category_id FROM Transactions INNER JOIN Products ON Products.ID = Transactions.product_id;";
                 
 
                 var transacoesT = await sqlConnection.QueryAsync<TransacaoProduto>(sql);
+
+                if (transacoesT != null)
+                {
+                    Console.WriteLine(transacoesT.ToString());
+
+                }
 
                 return Ok(transacoesT);
 
@@ -58,7 +62,7 @@ namespace Akshay.Controllers
 
         }
 
-        */
+        
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
